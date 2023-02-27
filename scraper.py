5c19af191e99d0a8acf1bfdeacc7527e6aa7a001
root <root@tenet.com>
2023-02-27 13:59:44 +0800

    update

--- scraper.py
+++ scraper.py
@@ -15,18 +15,23 @@ import time
 import win32com.client as win32
 from selenium.common.exceptions import NoSuchElementException
 import datetime
+import os
 
 
 #d1 = date.today().strftime('%d/%m/%Y')
 email_sent = 0
-keyword = 'FAILED'
+keyword = 'SUCCESS'
 url = 'url'
 
 options = webdriver.ChromeOptions()
 options.add_argument("--headless=new")
+options.add_argument("window-size=1980,960")
+options.add_argument('--disable-gpu')
+options.add_argument("screenshot")
 driver = webdriver.Chrome(options=options)
 driver.implicitly_wait(5)
 driver.get(url)
+driver.maximize_window()
 time.sleep(10)
 
 
@@ -35,8 +40,9 @@ def send_email():
     mail = outlook.CreateItem(0)
     mail.To = 'to_addr'
     mail.Subject = '[Action Required!] F2 process Failed'
-    mail.Body = 'One of the daily F2 tasks has failed. Please look into it asap'
-    # mail.HTMLBody = '<h2>HTML Message body</h2>' #this field is optional
+    #mail.Body = 'One of the daily F2 tasks has failed. Please look into it asap'
+    # this field is optional
+    mail.HTMLBody = 'One of the daily F2 tasks has failed. Please look into it asap <br><img src="C:\\temp\\screenshot.png">'
 
     # To attach a file to the email (optional):
     # attachment  = "Path to the attachment"
@@ -46,14 +52,16 @@ def send_email():
     return 1
 
 
-while datetime.datetime.now().hour < 13:
+while datetime.datetime.now().hour < 18:
     driver.refresh()
     if not email_sent:
         try:
             element = driver.find_element(
                 By.XPATH, f"//*[contains(text(),'{keyword}')]")
             if bool(element):
+                driver.save_screenshot('C:\\temp\\screenshot.png')
                 email_sent = send_email()
+                os.remove('C:\\temp\\screenshot.png')
         except NoSuchElementException as err:
             pass
     else:
