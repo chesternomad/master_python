c01b236f755acb561f4467d0338ffb36b901eb3e
root <root@tenet.com>
2023-03-02 14:40:07 +0800

    add new python

---
+++ safe.py
@@ -0,0 +1,36 @@
+import requests
+from selenium import webdriver
+from selenium.webdriver import Chrome
+from selenium.webdriver.common.by import By
+from webdriver_manager.chrome import ChromeDriverManager
+import time
+import win32com.client as win32
+from selenium.common.exceptions import NoSuchElementException
+
+key_list = ['accountsTradeable[]', 'commoditiesTradeable[]']
+
+url_list = ['https://app-safe-prod.azurewebsites.net/starGroups/?n=HKDESK',
+            'https://app-safe-prod.azurewebsites.net/starGroups/?n=HKDESK1']
+
+
+def search_value(team, key):
+    element = driver.find_element(By.NAME, key)
+
+    with open(f'C:\\temp\\{team}_{key[:-2]}.txt', 'w') as f:
+        f.write(element.text.encode("gbk", "ignore").decode("gbk", "ignore"))
+        f.close()
+    time.sleep(30)
+    return
+
+
+if __name__ == '__main__':
+    for i in url_list:
+        head, team = i.split('=')
+        driver = webdriver.Chrome()
+        driver.implicitly_wait(5)
+        driver.get(i)
+        # driver.maximize_window()
+        time.sleep(10)
+        for j in key_list:
+            search_value(team, j)
+        driver.quit()
