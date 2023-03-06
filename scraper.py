e9b58d7cff83adbb97442c4655c7f32d963a0382
Chester Yeung <96572773+chesternomad@users.noreply.github.com>
2023-03-06 10:57:42 +0800

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -18,8 +18,8 @@ import datetime
 import os
 
 email_sent = 0
-keyword = 'FAILED'
-url = 'url'
+# keyword = 'FAILED'
+url = 'http://f2sql-uks-p1w01/ReportServer/Pages/ReportViewer.aspx?%2FFuturesII-2%2FHong%20Kong%20Checks%2FScheduler%20Status&rc:showbackbutton=true'
 options = webdriver.ChromeOptions()
 options.add_argument("--headless=new")
 options.add_argument("window-size=1980,960")
@@ -35,11 +35,11 @@ time.sleep(10)
 def send_email(screenshot):
     outlook = win32.Dispatch('outlook.application')
     mail = outlook.CreateItem(0)
-    mail.To = 'to_addr'
+    mail.To = 'hkit@sucfin.com'
     mail.Subject = '[Action Required!] F2 process Failed'
     attachment = mail.Attachments.Add(screenshot)
     attachment.PropertyAccessor.SetProperty("http://schemas.microsoft.com/mapi/proptag/0x3712001F", "f2_screenshot")
-    mail.HTMLBody = 'One of the daily F2 tasks has failed. Please look into it asap <br><img src="cid:f2_screenshot">'
+    mail.HTMLBody = 'The latest running task has failed. Please look into it asap <br><img src="cid:f2_screenshot">'
     mail.Send()
     time.sleep(20)
     return 1
@@ -50,8 +50,8 @@ while datetime.datetime.now().hour < 13:
     if not email_sent:
         try:
             element = driver.find_element(
-                By.XPATH, f"//*[contains(text(),'{keyword}')]")
-            if bool(element):
+                By.XPATH, f"//*[contains(@id,'97iT0R0x0')]")
+            if element.text == 'FAILED':
                 screenshot = 'C:\\temp\\screenshot.png'
                 driver.save_screenshot(screenshot)
                 email_sent = send_email(screenshot)
@@ -60,5 +60,5 @@ while datetime.datetime.now().hour < 13:
             pass
     else:
         break
-time.sleep(180)
+    time.sleep(180)
 driver.quit()
